1139c771abf7323df642b573f67c18d1c6a72b06
Rub21 <rub2106@gmail.com>
2019-05-28 11:41:19 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const csv = require('csvtojson');
 const fs = require('fs');
-var path = process.argv[2];
+const path = process.argv[2];
+const numFiles = 1500;
 fs.readdir(path, function (err, items) {
-    if (items.length > 3) {
-        for (var i = 0; i < items.length; i++) {
-            const imagenNumber = items[i].split('.')[0];
+    if (items.length > numFiles) {
+        for (let i = numFiles; i < items.length; i++) {
+            const imagenNumber = items[i].split('.')[0].replace('_Cam3', '').replace('_Cam1', '');
             if (Number(imagenNumber)) {
-                // fs.unlinkSync(items[i]);
+                //fs.unlinkSync(items[i]);
                 console.log(`File ${items[i]} deleted!`);
             }
         }
